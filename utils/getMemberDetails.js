75e61f3d0280e80a0b15d0526e5bfd0d3748d7e0
zafajardo9 <zafajardo9.programming@gmail.com>
2024-06-10 20:09:20 +0800

    update

--- utils/getMemberDetails.js
+++ utils/getMemberDetails.js
@@ -2,10 +2,14 @@ import { wixClient } from '../wixClient.js';
 
 export async function getMemberData(memberId) {
   try {
-    const response = await wixClient.members.getMember(memberId);
+    const options = {
+      fieldsets: "FULL"
+    };
+    const response = await wixClient.members.getMember(memberId, options);
+
     return response;
   } catch (error) {
     console.error('Error getting member:', error);
-    throw error; 
+    throw error;
   }
 }
